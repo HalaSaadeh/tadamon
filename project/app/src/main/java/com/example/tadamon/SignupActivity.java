78ffb616ac8edefd0320f7ae056a0177ab675fe5
HalaSaadeh <halagsaadeh@gmail.com>
2020-11-26 22:07:46 +0200

    added firebase dependencies

--- project/app/src/main/java/com/example/tadamon/SignupActivity.java
+++ project/app/src/main/java/com/example/tadamon/SignupActivity.java
@@ -2,6 +2,8 @@ package com.example.tadamon;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SignupActivity extends AppCompatActivity {
 
     private TextView mTextView;
-
+    private Button emailpasssign;
+    private Button googlesign;
+    private Button facebooksign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
         mTextView = (TextView) findViewById(R.id.text);
+        emailpasssign = findViewById(R.id.emailpasssign);
+        googlesign = findViewById(R.id.googlesign);
+        facebooksign = findViewById(R.id.facebooksign);
+
+        emailpasssign.setOnClickListener(e->{});
+        googlesign.setOnClickListener(e->{});
+        facebooksign.setOnClickListener(e->{});
 
 
     }
