b9757abe651816d3da558d77634f40be8e69727e
HalaSaadeh <halagsaadeh@gmail.com>
2020-11-26 22:31:45 +0200

    added signup with email and pass

--- project/app/src/main/java/com/example/tadamon/SignupWithEmailAndPassActivity.java
+++ project/app/src/main/java/com/example/tadamon/SignupWithEmailAndPassActivity.java
@@ -11,4 +11,27 @@ public class SignupWithEmailAndPassActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_with_email_and_pass);
     }
+/*
+    private void signUpWithEmailAndPass(email, password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }*/
 }
