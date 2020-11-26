be7e19f3a19057aca921ad92cfb9ffe6f51c233b
HalaSaadeh <halagsaadeh@gmail.com>
2020-11-26 21:53:41 +0200

    added signup

--- project/app/src/main/java/com/example/tadamon/MainActivity.java
+++ project/app/src/main/java/com/example/tadamon/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tadamon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button signup = findViewById(R.id.button);
 
+        signup.setOnClickListener(e -> {
+            Intent startIntent = new Intent(getApplicationContext(), SignupActivity.class);
+            startActivity(startIntent);
+    });
     }
 }
