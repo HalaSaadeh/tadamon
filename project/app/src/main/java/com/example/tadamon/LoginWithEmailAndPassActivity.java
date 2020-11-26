8a25277598bac30d7ddfc04b839c793496d2fc8e
ea1ik <ea1ik@icloud.com>
2020-11-26 23:31:24 +0200

    added all signin/signup screens and homescreen

---
+++ project/app/src/main/java/com/example/tadamon/LoginWithEmailAndPassActivity.java
@@ -0,0 +1,14 @@
+package com.example.tadamon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginWithEmailAndPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_with_email_and_pass);
+    }
+}
