4fcf944be0451dbe334d610ebdb0aee6bff65237
HalaSaadeh <halagsaadeh@gmail.com>
2020-12-07 12:07:04 +0200

    finished user volunteer cards and crisis screen

--- project/app/src/main/java/com/example/tadamon/CrisisActivity.java
+++ project/app/src/main/java/com/example/tadamon/CrisisActivity.java
@@ -1,13 +1,33 @@
 package com.example.tadamon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 public class CrisisActivity extends AppCompatActivity {
 
@@ -17,8 +37,10 @@ public class CrisisActivity extends AppCompatActivity {
     private TextView donationCount, totalDonated, neededDonations;
     private ImageView donor1, donor2, donor3, donor4, donor5;
     private ImageView volunteer1, volunteer2, volunteer3, volunteer4, volunteer5;
+    private ImageView coverPhoto;
     private TextView totalDonators, totalVolunteers;
     private ProgressBar progressBar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance(); // get Instance of the Cloud Firestore database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +56,8 @@ public class CrisisActivity extends AppCompatActivity {
         crisisTitle = findViewById(R.id.crisisTitleCrisisActivity);
         crisisDaysLeft = findViewById(R.id.daysLeftCrisisActivity);
         crisisDesc = findViewById(R.id.crisisDescriptionCrisisActivity);
+        coverPhoto = findViewById(R.id.bgImageCrisis);
+        String name = getIntent().getStringExtra("id");
 
         donationCount = findViewById(R.id.donorNumberLabelCrisisActivity);
         totalDonated = findViewById(R.id.donationsCurrentLabelCrisisActivity);
@@ -56,5 +80,73 @@ public class CrisisActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.donationsProgressBar);
 
+        populateCrisis(name);
+
+    }
+    private void populateCrisis(String id){
+        DocumentReference eventRef = db.collection("crisis_events").document(id);
+        eventRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> data = document.getData();
+                        String eventname = (String) data.get("eventname");
+                        crisisTitle.setText(eventname);
+                        String cover_photo_url = (String) data.get("photo_url");
+                        setPic(cover_photo_url, coverPhoto);
+                        crisisDesc.setText((String) data.get("description"));
+                        long amountneeded = (long) data.get("amountneeded");
+                        neededDonations.setText( amountneeded + "M LBP");
+                        long amountraised = (long) data.get("amountraised");
+                        totalDonated.setText( amountraised + "M LBP");
+                        progressBar.setProgress((int) (amountraised/amountneeded * 100));
+                        List<String> donors = (List<String>) data.get("donorphotos");
+                        donationCount.setText(""+donors.size());
+                        if (donors.size()>5)
+                            totalDonators.setText("+"+(donors.size()-5)); // else hide the circle
+                        List<String> volunteers = (List<String>) data.get("volunteerphotos");
+                        if (volunteers.size()>5)
+                            totalVolunteers.setText("+"+(volunteers.size()-5)); // else hide the circle
+
+                        // Setting donor images
+                        ImageView[] donorImages = {donor1, donor2, donor3, donor4, donor5};
+                        for (int i=0; i<5; i++){
+                            if (i>=donors.size())
+                                break;
+                            setPic(donors.get(i), donorImages[i]);
+                        }
+                        // Setting volunteer images
+                        ImageView[] volunteerImages = {volunteer1, volunteer2, volunteer3, volunteer4, volunteer5};
+                        for (int i=0; i<5; i++){
+                            if (i>=volunteers.size())
+                                break;
+                            setPic(volunteers.get(i), volunteerImages[i]);
+                        }
+                    }
+                }}});
+    }
+    private static void setPic(String urlImage, ImageView imageView){
+        new AsyncTask<String, Integer, Drawable>(){
+            @Override
+            protected Drawable doInBackground(String... strings) {
+                Bitmap pic = null;
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(urlImage).openConnection();
+                    connection.connect();
+                    InputStream input = connection.getInputStream();
+                    pic = BitmapFactory.decodeStream(input);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return new BitmapDrawable(Resources.getSystem(), pic);
+            }
+            protected void onPostExecute(Drawable result) {
+
+                //Add image to ImageView
+                imageView.setImageDrawable(result);
+            }
+        }.execute();
     }
 }
--- project/app/src/main/java/com/example/tadamon/HomeScreenActivity.java
+++ project/app/src/main/java/com/example/tadamon/HomeScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.tadamon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -7,8 +8,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -20,24 +26,35 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class HomeScreenActivity extends AppCompatActivity implements PopUpMessage.LoggingOut {
 
     private Button signout;
     private FirebaseAuth mAuth;
-
+    private TextView welcomeLabel;
     private HorizontalScrollView scrollView;
-    private LinearLayout listOfUserCards, listOfDonationCards, listOfVolunteeringCards;
-
+    private LinearLayout listOfUserCards, listOfVolunteeringCards;
+    FirebaseFirestore db = FirebaseFirestore.getInstance(); // get Instance of the Cloud Firestore database
     public ArrayList<MaterialCardView> list1, list2, list3;
 
     @Override
@@ -50,8 +67,7 @@ public class HomeScreenActivity extends AppCompatActivity implements PopUpMessag
 
         listOfUserCards = findViewById(R.id.upToDateList);
         listOfVolunteeringCards = findViewById(R.id.volunteerList);
-        listOfDonationCards = findViewById(R.id.donationList);
-
+        welcomeLabel = findViewById(R.id.welcomeLabel);
         bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
             switch (menuItem.getItemId()) {
                 case R.id.discover:
@@ -74,37 +90,19 @@ public class HomeScreenActivity extends AppCompatActivity implements PopUpMessag
             openDialog();
         });
 
-        MaterialCardView card1 = createEventCard(1, "Event 1", R.drawable.bg_img);
-        MaterialCardView card2 = createEventCard(2, "Event 2", R.drawable.bg_img);
-        MaterialCardView card3 = createEventCard(3, "Event 3", R.drawable.bg_img);
 
-        MaterialCardView card4 = createEventCard(4, "Event 1", R.drawable.bg_img);
-        MaterialCardView card5 = createEventCard(5, "Event 2", R.drawable.bg_img);
-        MaterialCardView card6 = createEventCard(6, "Event 3", R.drawable.bg_img);
+        loadCrisisEvents();
 
-        MaterialCardView card7 = createEventCard(7, "Event 1", R.drawable.bg_img);
-        MaterialCardView card8 = createEventCard(8, "Event 2", R.drawable.bg_img);
-        MaterialCardView card9 = createEventCard(9, "Event 3", R.drawable.bg_img);
 
-        listOfUserCards.addView(card1);
-        listOfUserCards.addView(card2);
-        listOfUserCards.addView(card3);
-
-        listOfVolunteeringCards.addView(card4);
-        listOfVolunteeringCards.addView(card5);
-        listOfVolunteeringCards.addView(card6);
-
-        listOfDonationCards.addView(card7);
-        listOfDonationCards.addView(card8);
-        listOfDonationCards.addView(card9);
     }
 
-    public MaterialCardView createEventCard(int id, String title, int imgSrc) {
+    public MaterialCardView createEventCard(String id, String title, String imgSrc) {
         int margin = dpToPx(8);
         int padding = dpToPx(16);
 
         MaterialCardView card = new MaterialCardView(this);
-        card.setId(id);
+        //card.setId(id);
+        card.setTag(id);
 
         card.setLayoutParams(new LinearLayout.LayoutParams(
                 (int) getResources().getDimension(R.dimen.largeButtonWidth),
@@ -126,7 +124,10 @@ public class HomeScreenActivity extends AppCompatActivity implements PopUpMessag
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 dpToPx(200)
         ));
-        imageView.setImageDrawable(getResources().getDrawable(imgSrc, getApplicationContext().getTheme()));
+
+        //imageView.setImageDrawable(getResources().getDrawable(imgSrc, getApplicationContext().getTheme()));
+        setPic(imgSrc, imageView);
+        setPic(imgSrc, imageView);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
         LinearLayout innerLinearLayout = new LinearLayout(this);
@@ -156,7 +157,10 @@ public class HomeScreenActivity extends AppCompatActivity implements PopUpMessag
 
         // TODO -> NAVIGATE TO APPROPRIATE SCREEN WITH THE RIGHT INFO PULLED FROM THE DB
         card.setOnClickListener(e -> {
-            startActivity(new Intent(this, CrisisActivity.class));
+            Intent intent = new Intent(this, CrisisActivity.class);
+            Log.d("tag", card.getTag().toString());
+            intent.putExtra("id", card.getTag().toString());
+            startActivity(intent);
         });
 
         return card;
@@ -182,5 +186,71 @@ public class HomeScreenActivity extends AppCompatActivity implements PopUpMessag
 
     public static int dpToPx(int dp) {
         return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    private void loadCrisisEvents(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String userid = preferences.getString("ID", null);
+        DocumentReference docRef = db.collection("volunteers").document(userid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> data = document.getData();
+                        // map of key value pairs where
+                        // the string is the key and object can be any Firestore type
+                        String name = (String) data.get("name");
+                        welcomeLabel.setText("Hi, " + name);
+                        List<String> volunteered_in = (List<String>) data.get("volunteered_in");
+                        for(String vol_key : volunteered_in){
+                            loadEventData(vol_key);
+                        }
+
+                    }
+                } else {
+                    Toast.makeText(HomeScreenActivity.this, "An error occurred. Please try again.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private void loadEventData(String vol_key){
+        DocumentReference eventRef = db.collection("crisis_events").document(vol_key);
+        eventRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> data = document.getData();
+                        String eventname = (String) data.get("eventname");
+                        String cover_photo_url = (String) data.get("photo_url");
+                        listOfUserCards.addView(createEventCard(vol_key, eventname, cover_photo_url));
+                    }
+                }}});
+    }
+    private static void setPic(String urlImage, ImageView imageView){
+        new AsyncTask<String, Integer, Drawable>(){
+            @Override
+            protected Drawable doInBackground(String... strings) {
+                Bitmap pic = null;
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(urlImage).openConnection();
+                    connection.connect();
+                    InputStream input = connection.getInputStream();
+                    pic = BitmapFactory.decodeStream(input);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return new BitmapDrawable(Resources.getSystem(), pic);
+            }
+            protected void onPostExecute(Drawable result) {
+
+                //Add image to ImageView
+                imageView.setImageDrawable(result);
+            }
+        }.execute();
     }
 }
