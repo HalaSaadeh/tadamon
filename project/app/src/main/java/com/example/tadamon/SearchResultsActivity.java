618ea7e70b9b46ae2f4c7ebeb0658fd00c6d4cce
ea1ik <ea1ik@icloud.com>
2020-12-12 01:11:56 +0200

    added remove tags

--- project/app/src/main/java/com/example/tadamon/SearchResultsActivity.java
+++ project/app/src/main/java/com/example/tadamon/SearchResultsActivity.java
@@ -1,19 +1,38 @@
 package com.example.tadamon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.card.MaterialCardView;
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SearchResultsActivity extends AppCompatActivity {
 
     private TextView criteriaLabel;
     private String searchCriteria;
+    private LinearLayout searchResultList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +63,109 @@ public class SearchResultsActivity extends AppCompatActivity {
         searchCriteria = intent.getStringExtra("criteria");
         criteriaLabel.setText(criteriaLabel.getText() + searchCriteria);
 
+        searchResultList = findViewById(R.id.searchResultList);
+
+        searchResultList.addView(createEventCard("nocnwie", "Event", "R.drawable.bg_img"));
+        searchResultList.addView(createEventCard("nocnwie", "Event", "R.drawable.bg_img"));
+        searchResultList.addView(createEventCard("nocnwie", "Event", "R.drawable.bg_img"));
+
+
+
+    }
+
+    public MaterialCardView createEventCard(String id, String title, String imgSrc) {
+        int margin = dpToPx(8);
+        int padding = dpToPx(16);
+
+        MaterialCardView card = new MaterialCardView(this);
+        //card.setId(id);
+        card.setTag(id);
+
+        card.setLayoutParams(new LinearLayout.LayoutParams(
+                (int) getResources().getDimension(R.dimen.largeButtonWidth),
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) card.getLayoutParams();
+        marginLayoutParams.setMargins(margin, margin, margin, margin);
+        card.setElevation(8);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                dpToPx(200)
+        ));
+
+        //imageView.setImageDrawable(getResources().getDrawable(imgSrc, getApplicationContext().getTheme()));
+        setPic(imgSrc, imageView);
+        setPic(imgSrc, imageView);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        LinearLayout innerLinearLayout = new LinearLayout(this);
+        innerLinearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                dpToPx(60)
+        ));
+        innerLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        innerLinearLayout.setPadding(padding, padding, padding, padding);
+
+
+        TextView text = new TextView(this);
+        text.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        text.setText(title);
+        text.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.largeTextSize));
+        text.setTextColor(getResources().getColor(R.color.black, getApplicationContext().getTheme()));
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.poppins_light);
+        text.setTypeface(typeface);
+
+        innerLinearLayout.addView(text);
+        linearLayout.addView(imageView);
+        linearLayout.addView(innerLinearLayout);
+        card.addView(linearLayout);
+
+        // TODO -> NAVIGATE TO APPROPRIATE SCREEN WITH THE RIGHT INFO PULLED FROM THE DB
+        card.setOnClickListener(e -> {
+            Intent intent = new Intent(this, CrisisActivity.class);
+            intent.putExtra("id", card.getTag().toString());
+            startActivity(intent);
+        });
+
+        return card;
+    }
+
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    private static void setPic(String urlImage, ImageView imageView){
+        new AsyncTask<String, Integer, Drawable>(){
+            @Override
+            protected Drawable doInBackground(String... strings) {
+                Bitmap pic = null;
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(urlImage).openConnection();
+                    connection.connect();
+                    InputStream input = connection.getInputStream();
+                    pic = BitmapFactory.decodeStream(input);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return new BitmapDrawable(Resources.getSystem(), pic);
+            }
+            protected void onPostExecute(Drawable result) {
+
+                //Add image to ImageView
+                imageView.setImageDrawable(result);
+            }
+        }.execute();
     }
 }
